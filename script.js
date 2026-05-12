d2d6b0c12e016c27572fbef21b9b04265d1d18c3
DominikGrom <40479@student.atar.edu.pl>
2026-05-12 12:27:25 +0200

    feat:dodanie sekcji o mnie + JavaScript i CSS

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+const opis = document.getElementById("opis");
+
+button.addEventListener("click", function () {
+    opis.innerHTML = "Kliknąłeś przycisk! Uczę się JavaScript";
+});
